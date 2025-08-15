9cfa76008b927b72e98fda4f8aba853025c64193
matheus00707 <matheusaomoicano@gmail.com>
2025-08-15 11:45:36 -0300

    css-artigo

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+// App.js
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="cartoes">
+      <div className="cartao">
+      
+        <h2>Como aprender CSS rápido</h2>
+        <p>Dicas práticas para melhorar suas habilidades.</p>
+        <button>Ler mais</button>
+      </div>
+
+      <div className="cartao">
+        <h2>Flexbox vs Grid</h2>
+        <p>Quando usar cada um para criar layouts.</p>
+        <button>Ler mais</button>
+      </div>
+
+      <div className="cartao">
+        <h2>Animações em CSS</h2>
+        <p>Como deixar seu site mais interativo.</p>
+        <button>Ler mais</button>
+      </div>
+    </div>
+  );
+}
